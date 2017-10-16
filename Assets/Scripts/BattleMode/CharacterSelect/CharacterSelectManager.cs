3af44f9909f465bb9fc0ad783c723ae1cf9c9dd2
flanny7 <mottei.senshu@gmail.com>
2017-10-16 16:43:18 +0900

    クー・チャンのUIシステムを受け継いだ（クラス名変わってますorz）

---
+++ Assets/Scripts/BattleMode/CharacterSelect/CharacterSelectManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// f:CharacterSelectManager(CharacterSelectModes)
+// f:CharacterSelectScene(Battle)において、シーン遷移を担うクラス
+// f:by flanny 2017/10/16
+public class CharacterSelectManager : MonoBehaviour {
+	public static string player1Character;
+
+	// Use this for initialization
+
+	public void SelectChara1 () {
+		player1Character = "";
+		SceneManager.LoadScene ("GameUI");
+	}	
+
+	public void SelectChara2(){
+		SceneManager.LoadScene ("GameUI");
+	}
+
+	public void SelectChara3(){
+		SceneManager.LoadScene ("GameUI");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ModeSelectScene/ModeSelecter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// f:ModeSelecter(SelectSelectModes)
+// f:ModeSelecter、シーン遷移を担うクラス
+// f:by flanny 2017/10/16
+public class ModeSelecter : MonoBehaviour {
+	public void SoloStart () {
+		SceneManager.LoadScene ("CharacterSelect_BattleMode");
+	}	
+
+	public void BattleStart(){
+		SceneManager.LoadScene ("CharacterSelect_BattleMode");
+	}
+
+	public void SettingStart(){
+		SceneManager.LoadScene ("Setting");
+	}
+}
