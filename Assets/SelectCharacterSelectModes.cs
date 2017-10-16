aad35ac37f3a6460d2703d019fda9bba537af4ef
Qoo238 <30618931+Qoo238@users.noreply.github.com>
2017-10-16 10:00:09 +0900

    Add files via upload
    よくわかんないけどこれでできてたら嬉しいpart2

---
+++ Assets/SelectCharacterSelectModes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectCharacterSelectModes : MonoBehaviour {
+	public static string player1Character;
+
+	// Use this for initialization
+
+	public void SelectChara1 () {
+		player1Character = "";
+		SceneManager.LoadScene ("GameUI");
+	}	
+
+	public void SelectChara2(){
+		SceneManager.LoadScene ("GameUI");
+	}
+
+	public void SelectChara3(){
+		SceneManager.LoadScene ("GameUI");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TitleSelectModes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSelectModes : MonoBehaviour {
+	void start(){
+	}
+
+	public void GameStart () {
+		SceneManager.LoadScene ("SelectMode");
+	}	
+
+	public void EndGame () {
+		UnityEditor.EditorApplication.isPlaying = false;
+	}
+}
