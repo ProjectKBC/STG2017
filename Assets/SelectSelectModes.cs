25936e64258806ab02103b01ababd0fe2e602e05
Qoo238 <30618931+Qoo238@users.noreply.github.com>
2017-10-16 09:57:57 +0900

    Add files via upload
    よくわかんないけどこれでできてたらめちゃ嬉しい

---
+++ Assets/SelectSelectModes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectSelectModes : MonoBehaviour {
+	public void SoloStart () {
+		SceneManager.LoadScene ("SelectCharacter");
+	}	
+
+	public void ButttleStart(){
+		SceneManager.LoadScene ("SelectCharacter");
+	}
+
+	public void SettingStart(){
+		SceneManager.LoadScene ("Setting");
+	}
+}
